03bf4e628cccce35e0780646e5f3aff7e7047190
Syeda Marium Younus <mariumyounus4@gmail.com>
2021-03-24 00:18:27 +1100

    Clock

---
+++ index.js
@@ -0,0 +1,20 @@
+const secondHand=document.querySelector('.sec-hand');
+const minHand = document.querySelector(".min-hand");
+const hourHand = document.querySelector(".hour-hand");
+function setDate()
+{
+    const now= new Date();
+    const seconds= now.getSeconds();
+    const secondsDegrees =((seconds/60)*360 + 90);
+    secondHand.style.transform=`rotate(${secondsDegrees}deg)`;
+    console.log(seconds);
+    const minutes = now.getMinutes();
+    const minDegrees=((minutes/60)*360 + 90);
+    minHand.style.transform = `rotate(${minDegrees}deg)`;
+    console.log(minutes);
+const hours = now.getHours();
+const hourDegrees = ((hours / 12) * 360 + 90);
+hourHand.style.transform = `rotate(${hourDegrees}deg)`;
+console.log(hours);
+}
+setInterval(setDate,1000);
